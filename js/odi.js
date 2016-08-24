51d3eea912d3644efb36d96908f5ecf8da10362b
Dave Tarrant <davetaz@theodi.org>
2016-08-24 18:10:21 +0100

    Adding code to track saving status and add email

--- js/odi.js
+++ js/odi.js
@@ -2,7 +2,8 @@ define(function(require) {
 	
 	var Adapt = require('coreJS/adapt');
 	var Backbone = require('backbone');
-	var ThemeBlock = require('theme/adapt-theme-odi2/js/theme-block');
+	var ThemeBlock = require('theme/adapt-theme-odi/js/theme-block');
+	var emailPresent = false;
 
 	// Block View
 	// ==========
@@ -19,4 +20,130 @@ define(function(require) {
 			});
 		}
 	});
+
+	Adapt.on('emailInput:updated', function() {
+		emailSave();
+		emailPresent = true;
+	});
+
+	Adapt.on('trackingHub:saving', function() {
+		if (!emailPresent) { return; }
+		$('#save-section').addClass('saving');
+		var sl = document.getElementById('save-section');
+		var toClass = "cloud_saving";
+		$(sl).css('background-image','url(adapt/css/assets/' + toClass + '.gif)');
+	});
+
+	Adapt.on('trackingHub:success', function() {
+		if (!emailPresent) { return; }
+		$('#save-section').addClass('success');
+		var sl = document.getElementById('save-section');
+		var toClass = "cloud_success";
+		$(sl).css('background-image','url(adapt/css/assets/' + toClass + '.gif)');
+	});
+	
+	Adapt.on('trackingHub:failed', function() {
+		if (!emailPresent) { return; }
+		$('#save-section').addClass('failed');
+		var sl = document.getElementById('save-section');
+		var toClass = "cloud_failed";
+		$(sl).css('background-image','url(adapt/css/assets/' + toClass + '.gif)');
+	});
+
+	// Custom bits
+	// ============
+	var click_bind = false;
+
+	function showMessage(phraseId) {
+		console.log("In show message");
+		
+		var alertObject = {
+            title: "Save your progress, earn rewards...",
+            body: "<p>Please enter your <b>email</b> address in the box below. You will receive an email linking to your unique profile so you can save your progress, earn rewards and resume your learning on any device.</p><br/><div align='center'><input type='email' id='email' class='email-input' placeholder='Email address' required></input><br/><br/><input type='submit' id='email_submit' value='OK' style='padding: 10px;' class='notify-popup-done course_link' role='button' aria-label='submit email' onClick='getEmail();'></input></div>"
+        };
+        
+        Adapt.once("notify:closed", function() {
+            Adapt.trigger("tutor:closed");
+        });
+
+        Adapt.trigger('notify:popup', alertObject);
+
+        Adapt.trigger('tutor:opened');
+	}
+
+	function addListeners() {
+		if (!click_bind) {
+			$('.save-section-outer').click(function() {
+				$('#cloud-status').slideToggle();
+			});
+			click_bind = true;
+		}
+		$('#saveSession').click(function() {
+			showMessage();
+		});
+	}
+
+	function emailSave() {
+		$('#save-section').fadeOut( function() {
+    		var sl = document.getElementById('save-section');
+			var ss = document.getElementById('cloud-status-text');
+			$(sl).html("");
+			$(sl).addClass('saving');
+			var toClass = "cloud_saving";
+			$(sl).css('background-image','url(adapt/css/assets/' + toClass + '.gif)');
+			$(sl).fadeIn();
+		});
+	}
+
+
+	function checkWelcome() {
+		user = localStorage.getItem('user');
+		if (!user.email && !localStorage.getItem("ODI_Welcome_Done")) {
+			showMessage('enter_email');
+			//localStorage.setItem("ODI_Welcome_Done",true);
+		}
+	}
+
+	function checkState() {
+		var sessionID = localStorage.getItem("UserID");
+		user = $.parseJSON(localStorage.getItem('user'));
+		if (user) { 
+			var sessionEmail = user.email; 
+			var lastSave = user.lastSave;	
+		}
+		if (!sessionEmail && sessionID) {
+			emailPresent = false;
+			checkWelcome();
+			$('#save-section').html("<button class='slbutton' id='saveSession'>Save progress</button>");
+			$('#save-section').fadeIn();
+			click_bind = false;
+			$('.save-section-outer').unbind('click');
+			addListeners();
+		} else {
+			emailPresent = true;
+			$('#save-section').fadeIn();
+			addListeners();
+		}
+	}
+
+	Adapt.on('pageView:postRender', function(view) {
+		checkState();
+	});
+
 });
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function getEmail() {
+	var Adapt = require('coreJS/adapt');
+	email = $("input[id='email']").val();
+	if (validateEmail(email)) {
+		user = $.parseJSON(localStorage.getItem('user'));
+		user.email = email;
+		localStorage.setItem('user',JSON.stringify(user));
+		Adapt.trigger('emailInput:updated');
+	}
+}
